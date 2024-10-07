9faeb3ca1644210eb15b4df12edc49ce50a9d3c6
mwarevn <mwarevn@gmail.com>
2024-10-07 16:21:50 +0700

    oke

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,6 +9,8 @@ import { PrismaModule } from './modules/prisma/prisma.module';
 import { PrismaService } from 'src/modules/prisma/prisma.service';
 import { ComboModule } from './modules/combo/combo.module';
 import { ServiceModule } from './modules/service/service.module';
+import { StylistModule } from './modules/stylist/stylist.module';
+import { CustomerModule } from './modules/customer/customer.module';
 
 @Module({
   imports: [
@@ -19,6 +21,8 @@ import { ServiceModule } from './modules/service/service.module';
     PrismaModule,
     ComboModule,
     ServiceModule,
+    StylistModule,
+    CustomerModule,
   ],
   controllers: [AppController],
   providers: [AppService, PrismaService],
