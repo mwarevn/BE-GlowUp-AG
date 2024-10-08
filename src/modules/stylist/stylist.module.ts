1c399602dd3e30dd66b7290ce7265df9234dc26e
mwarevn <mwarevn@gmail.com>
2024-10-08 08:22:53 +0700

    get all stylist

--- src/modules/stylist/stylist.module.ts
+++ src/modules/stylist/stylist.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { StylistService } from './stylist.service';
 import { StylistController } from './stylist.controller';
+import { UserService } from 'src/modules/user/user.service';
+import { UserModule } from 'src/modules/user/user.module';
 
 @Module({
   controllers: [StylistController],
   providers: [StylistService],
+  imports: [UserModule],
 })
 export class StylistModule {}
